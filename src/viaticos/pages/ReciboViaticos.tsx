5916e8a03193c3344fc22efc5073c3e2d58c8c7b
Jorge <jorge.alfonso.casarez.palafox@uabc.edu.mx>
2023-02-16 09:18:28 -0800

    Se realizaron los ajustes

--- src/viaticos/pages/ReciboViaticos.tsx
+++ src/viaticos/pages/ReciboViaticos.tsx
@@ -53,12 +53,12 @@ export const ReciboViaticos = () => {
             <div className="container mt-4">
                 <div className="row">
                     <div className="col-md-6 border">
-                        <div className="p-2"><span>BUENO POR: </span><span><b>{`$${ formatoComision.importe.toFixed(2)}`}</b></span></div>
+                        <div className="p-2"><span className="gris">BUENO POR: </span><span><b>{`$${ formatoComision.importe.toFixed(2)}`}</b></span></div>
                     </div>
 
                     <div className="col border">
-                        <div className="p-2">NO. DE OFICIO: <span><b>{`V${formatoComision.oficina}-${ formatoComision.noViat }/${ formatoComision.ejercicio }`}</b></span></div>
-                        <div className="p-2">FECHA: <span>{ `${fechaViatico.getDate().toString()} DE ${ getMes( fechaViatico )} DE ${ formatoComision.ejercicio }` }</span></div>
+                        <div className="p-2"> <span className="gris">NO. DE OFICIO: </span> <span><b>{`V${formatoComision.oficina}-${ formatoComision.noViat }/${ formatoComision.ejercicio }`}</b></span></div>
+                        <div className="p-2"><span className="gris">FECHA: </span><span>{ `${fechaViatico.getDate().toString()} DE ${ getMes( fechaViatico )} DE ${ formatoComision.ejercicio }` }</span></div>
                     </div>
                     
                 </div>
@@ -66,9 +66,9 @@ export const ReciboViaticos = () => {
             <div className="container mt-4">
                 <div className="row">
                     <div className="col-3 border">
-                        <div className="p-2">NOMBRE:</div>
-                        <div className="p-2">DEPARTAMENTO:</div>
-                        <div className="p-2">PUESTO:</div>
+                        <div className="p-2 gris">NOMBRE:</div>
+                        <div className="p-2 gris">DEPARTAMENTO:</div>
+                        <div className="p-2 gris">PUESTO:</div>
                     </div>
                     <div className="col border">
                         <div className="p-2">{`${formatoComision.nombre} ${formatoComision.paterno} ${formatoComision.materno} ( ${formatoComision.noEmp})`}</div>
@@ -79,15 +79,15 @@ export const ReciboViaticos = () => {
             </div>
             <div className="container mt-4">
                 <div className="row border">
-                    <div className="p-2 text-center">DATOS DE LA COMISION</div>
+                    <div className="p-2 text-center gris">DATOS DE LA COMISION</div>
                 </div>
                 <div className="row border">
-                    <div className="col text-center datos-comision-titulos">ORIGEN</div>
-                    <div className="col text-center datos-comision-titulos">DESTINO</div>
-                    <div className="col text-center datos-comision-titulos">FECHA INICIO</div>
-                    <div className="col text-center datos-comision-titulos">FECHA TERMINO</div>
-                    <div className="col text-center datos-comision-titulos">DIAS</div>
-                    <div className="col text-center datos-comision-titulos">IMPORTE</div>
+                    <div className="col text-center datos-comision-titulos gris">ORIGEN</div>
+                    <div className="col text-center datos-comision-titulos gris">DESTINO</div>
+                    <div className="col text-center datos-comision-titulos gris">FECHA INICIO</div>
+                    <div className="col text-center datos-comision-titulos gris">FECHA TERMINO</div>
+                    <div className="col text-center datos-comision-titulos gris">DIAS</div>
+                    <div className="col text-center datos-comision-titulos gris">IMPORTE</div>
                 </div>
                 <div className="row border p-2">
                     <div className="col text-center">{`${formatoComision.cdOrigen}`} <br /> {`${formatoComision.edoOrigen}`}</div>
@@ -98,8 +98,8 @@ export const ReciboViaticos = () => {
                     <div className="col text-center">${`${formatoComision.importe.toFixed(2)}`} </div>
                 </div>
                 <div className="row border">
-                    <div className="col-3 text-center">MOTIVO</div>
-                    <div className="col text-center">ACTIVIDADES</div>
+                    <div className="col-3 text-center gris">MOTIVO</div>
+                    <div className="col text-center gris">ACTIVIDADES</div>
                 </div>
                 <div className="row border">
                     <div className="col-5 p-4 text-justify">{`${formatoComision.motivo}`} </div>
@@ -108,8 +108,8 @@ export const ReciboViaticos = () => {
             </div>
             <div className="container mt-4">
                 <div className="row border">
-                    <div className="col p-1 text-center">AUTORIZO</div>
-                    <div className="col p-1 text-center">RECIBI</div>
+                    <div className="col p-1 text-center gris">AUTORIZO</div>
+                    <div className="col p-1 text-center gris">RECIBI</div>
                 </div>
                 <div className="row border">
                     <div className="col mt-5 d-flex flex-column align-items-center">
