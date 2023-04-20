ee7502083a389e2c966047ccd7bdebe7cd137849
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-04-20 13:03:45 -0700

    LoginPage

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -76,13 +76,13 @@ export const LoginPage = () => {
                 </div>
             </div>
 
-            <div className="row d-flex justify-content-center">
+            {/* <div className="row d-flex justify-content-center">
                 <div className="col-md-6">
                     <div className="error-message mt-5">
                         <p className="text-center">{ status }</p>
                     </div> 
                 </div>
-            </div>
+            </div> */}
         </div>
     </AuthLayout>
     
